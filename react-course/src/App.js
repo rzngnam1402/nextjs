684216eb06dd53fa7135855ad66ef62f79c92189
rzngnam1402 <legiangnam1402@gmail.com>
2022-04-28 23:53:22 +0700

    create wrapper

--- react-course/src/App.js
+++ react-course/src/App.js
@@ -3,21 +3,24 @@ import { Route, Switch } from 'react-router-dom'
 import AllMeetups from './pages/AllMeetups';
 import Favorites from './pages/Favorites';
 import NewMeetup from './pages/NewMeetup';
+import Layout from './components/layout/Layout'
 
 function App() {
   return (
     <div>
-      <Switch>
-        <Route path='/' exact>
-          <AllMeetups />
-        </Route>
-        <Route path='/new-meetup' exact>
-          <NewMeetup />
-        </Route>
-        <Route path='/favorites' exact>
-          <Favorites />
-        </Route>
-      </Switch>
+      <Layout>
+        <Switch>
+          <Route path='/' exact>
+            <AllMeetups />
+          </Route>
+          <Route path='/new-meetup' exact>
+            <NewMeetup />
+          </Route>
+          <Route path='/favorites' exact>
+            <Favorites />
+          </Route>
+        </Switch>
+      </Layout>
     </div>
   );
 }
