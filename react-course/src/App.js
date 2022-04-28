097229317426357ac2a2f9fdff2f359dd4d6d2d9
rzngnam1402 <legiangnam1402@gmail.com>
2022-04-28 22:52:25 +0700

    add react router

--- react-course/src/App.js
+++ react-course/src/App.js
@@ -1,12 +1,23 @@
-import Backdrop from "./components/Backdrop";
-import Todo from "./components/Todo";
+import { Route, Switch } from 'react-router-dom'
+
+import AllMeetups from './pages/AllMeetups';
+import Favorites from './pages/Favorites';
+import NewMeetup from './pages/NewMeetup';
 
 function App() {
   return (
     <div>
-      <h1>My Todos</h1>
-      <Todo text="Learn React" />
-      <Todo text="Learn Nextjs" />
+      <Switch>
+        <Route path='/' exact>
+          <AllMeetups />
+        </Route>
+        <Route path='/new-meetup' exact>
+          <NewMeetup />
+        </Route>
+        <Route path='/favorites' exact>
+          <Favorites />
+        </Route>
+      </Switch>
     </div>
   );
 }
---
+++ react-course/src/pages/AllMeetups.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AllMeetups = () => {
+    return (
+        <div>AllMeetups</div>
+    )
+}
+
+export default AllMeetups
