a8e925a0bed5b72049d09b0f69b1e67114594613
rzngnam1402 <legiangnam1402@gmail.com>
2022-05-01 16:32:37 +0700

    send a post http request

--- react-course/src/components/meetups/NewMeetupForm.js
+++ react-course/src/components/meetups/NewMeetupForm.js
@@ -2,7 +2,7 @@ import React, { useRef } from 'react'
 import Card from '../ui/Card'
 import classes from './NewMeetupForm.module.css'
 
-const NewMeetupForm = () => {
+const NewMeetupForm = (props) => {
     const titleInputRef = useRef();
     const imageInputRef = useRef();
     const addressInputRef = useRef();
@@ -23,7 +23,7 @@ const NewMeetupForm = () => {
             description: enteredDescription,
         }
 
-        console.log(meetupData);
+        props.onAddMeetup(meetupData);
     }
 
     return (
