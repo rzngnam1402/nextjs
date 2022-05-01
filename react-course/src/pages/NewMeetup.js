f986c9da6d69d2b4df1d0837c9912ee1ac46305f
rzngnam1402 <legiangnam1402@gmail.com>
2022-05-01 17:01:13 +0700

    fetch using useEffect

--- react-course/src/pages/NewMeetup.js
+++ react-course/src/pages/NewMeetup.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import NewMeetupForm from '../components/meetups/NewMeetupForm'
+import { useHistory } from 'react-router-dom'
 
 const NewMeetup = () => {
+    const history = useHistory();
     const addMeetupHandler = (meetupData) => {
         fetch(
             'https://react-course-d988b-default-rtdb.firebaseio.com/meetups.json',
@@ -12,9 +14,10 @@ const NewMeetup = () => {
                     'Content-Type': 'application/json'
                 }
             }
-        );
+        ).then(() => {
+            history.replace('/');
+        });
     }
-
 
     return (
         <section>
