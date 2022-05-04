a9d52ff29afa7742c402e4897c9a3c86763d0d86
rzngnam1402 <legiangnam1402@gmail.com>
2022-05-04 11:34:11 +0700

    finish favorites page

--- react-course/src/pages/Favorites.js
+++ react-course/src/pages/Favorites.js
@@ -1,8 +1,24 @@
+import { useContext } from 'react'
 import React from 'react'
+import FavoritesContext from '../store/favorites-context'
+import MeetupList from '../components/meetups/MeetupList'
 
 const Favorites = () => {
+    const favoriteCtx = useContext(FavoritesContext);
+
+    let content;
+    if (favoriteCtx.totalFavorites === 0) {
+        content = <p>You got no favorites yet. Start adding some ?</p>;
+    }
+    else {
+        content = <MeetupList meetups={favoriteCtx.favorites}></MeetupList>;
+    }
+
     return (
-        <div>New Favorites</div>
+        <section>
+            <h1>My Favorites</h1>
+            {content}
+        </section>
     )
 }
 
