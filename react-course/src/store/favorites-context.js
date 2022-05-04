42b7ee608c6a7e9632e5e11d00927c21d3c92b8a
rzngnam1402 <legiangnam1402@gmail.com>
2022-05-04 10:37:47 +0700

    create context handling fuction

--- react-course/src/store/favorites-context.js
+++ react-course/src/store/favorites-context.js
@@ -7,6 +7,22 @@ const FavoritesContext = createContext({
 
 function FavoritesContextProvider(props) {
 
+    const addFavoritesHandler = (favoriteMeetup) => {
+        setUserFavorites((prevUserFavorites) => {
+            return prevUserFavorites.concat(favoriteMeetup);
+        })
+    }
+
+    const removeFavoritesHandler = (meetupId) => {
+        setUserFavorites((prevUserFavorites) => {
+            return prevUserFavorites.filter(meetup => meetup.id !== meetupId);
+        })
+    }
+
+    const itemIsFavoritesHandler = (meetupId) => {
+        return userFavorites.some(meetup => meetup.id === meetupId);
+    }
+
     const [userFavorites, setUserFavorites] = useState([]);
     const context = {
         favorites: userFavorites,
