317977304f9bd47e6cde79fede74e1b53361b3c0
rzngnam1402 <legiangnam1402@gmail.com>
2022-05-07 00:23:21 +0700

    using link dynamically

--- nextjs-course/pages/clients/index.js
+++ nextjs-course/pages/clients/index.js
@@ -1,8 +1,26 @@
+import Link from 'next/link'
 import React from 'react'
 
 const ClientsPage = () => {
+  const clients = [
+    { id: 'max', name: 'maximilian' },
+    { id: 'manu', name: 'manuel' },
+
+  ]
+
   return (
-    <div>ClientsPage</div>
+    <div>
+      <h1>
+        The Clients Page
+      </h1>
+      <ul>
+        {
+          clients.map(client => <li key={client.id}>
+            <Link href={`/clients/${client.id}`}>{client.name}</Link>
+          </li>)
+        }
+      </ul>
+    </div>
   )
 }
 
