1c754b617a6783b48b68c5d5b24ae40bde30dbdf
rzngnam1402 <legiangnam1402@gmail.com>
2022-05-12 09:22:16 +0700

    add styles

--- nextjs-course/components/events/event-item.js
+++ nextjs-course/components/events/event-item.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Link from 'next/link'
+import classes from './event-item.module.css'
 
 const EventItem = (props) => {
 
@@ -15,19 +16,19 @@ const EventItem = (props) => {
     const exploreLink = `/events/${id}`
 
     return (
-        <li>
+        <li className={classes.item}>
             <img src={'/' + image} alt={title}></img>
-            <div>
-                <div>
+            <div className={classes.content}>
+                <div className={classes.summary}>
                     <h2>{title}</h2>
-                    <div>
+                    <div className={classes.date}>
                         <time>{humanReadableDate}</time>
                     </div>
-                    <div>
+                    <div className={classes.address}>
                         <address>{formattedAddress}</address>
                     </div>
                 </div>
-                <div>
+                <div className={classes.actions}>
                     <Link href={exploreLink}>Explore Event</Link>
                 </div>
             </div>
