3fba802621a960fcd2a3ff71b86d52f8547ffc7c
rzngnam1402 <legiangnam1402@gmail.com>
2022-05-16 23:13:12 +0700

    navigating to filtered events

--- nextjs-course/components/events/event-search.js
+++ nextjs-course/components/events/event-search.js
@@ -1,21 +1,33 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import Button from '../ui/button'
 import classes from './event-search.module.css'
 
 const EventSearch = (props) => {
+    const yearInputRef = useRef();
+    const monthInputRef = useRef();
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+
+        const selectedYear = yearInputRef.current.value;
+        const selectedMonth = monthInputRef.current.value;
+
+        props.onSearch(selectedYear, selectedMonth);
+    }
+
     return (
-        <form className={classes.form}>
+        <form className={classes.form} onSubmit={submitHandler}>
             <div className={classes.controls}>
                 <div className={classes.control}>
                     <label htmlFor='year'>year</label>
-                    <select id="year">
+                    <select id="year" ref={yearInputRef}>
                         <option value='2021'>2021</option>
                         <option value='2022'>2022</option>
                     </select>
                 </div>
                 <div className={classes.control}>
                     <label htmlFor="month">Month</label>
-                    <select id="month">
+                    <select id="month" ref={monthInputRef}>
                         <option value="1">January</option>
                         <option value="2">February</option>
                         <option value="3">March</option>
