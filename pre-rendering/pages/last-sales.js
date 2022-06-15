3c92d973e478b35d4aad559d42f523ce5a491e59
rzngnam1402 <legiangnam1402@gmail.com>
2022-06-15 08:09:05 +0700

    change

--- pre-rendering/pages/last-sales.js
+++ pre-rendering/pages/last-sales.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import useSWR from 'swr';
 
-const LastSalesPage = () => {
-    const [sales, setSales] = useState();
+const LastSalesPage = (props) => {
+    const [sales, setSales] = useState(props.sales);
     // const [isLoading, setisLoading] = useState(false);
 
     const { data, error } = useSWR('https://nextjs-course-2d22f-default-rtdb.asia-southeast1.firebasedatabase.app/Sales.json',
@@ -44,7 +44,7 @@ const LastSalesPage = () => {
         return <p>Failed to load</p>
     }
 
-    if (!data || !sales) {
+    if (!data && !sales) {
         return <p>Loading...</p>
     }
 
@@ -53,6 +53,23 @@ const LastSalesPage = () => {
             {sales.map(sale => <li key={sale.id}>{sale.username} - ${sale.volume}</li>)}
         </ul>
     )
+}
+
+export async function getStaticProps() {
+    const response = await fetch('https://nextjs-course-2d22f-default-rtdb.asia-southeast1.firebasedatabase.app/Sales.json',
+        (url) => fetch(url).then((res) => res.json()))
+    const data = await response.json();
+
+    const transformedSales = [];
+    for (const key in data) {
+        transformedSales.push({
+            id: key,
+            username: data[key].username,
+            volume: data[key].volume
+        })
+    }
+
+    return { props: { sales: transformedSales }, revalidate: 10 }
 }
 
 export default LastSalesPage
