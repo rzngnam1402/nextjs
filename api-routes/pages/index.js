67666f4f05bffe557282a6e928ad93fdfe9c7a18
rzngnam1402 <legiangnam1402@gmail.com>
2022-06-16 11:29:29 +0700

    prepare frontend form

--- api-routes/pages/index.js
+++ api-routes/pages/index.js
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useRef } from 'react'
 
 const HomePage = () => {
+  const emailInputRef = useRef();
+  const feedbackInputRef = useRef();
+
+  const submitFormHandler = (event) => {
+    event.preventDefault();
+    const enteredEmail = emailInputRef.current.value;
+    const enteredFeedback = feedbackInputRef.current.value;
+    
+  }
+
   return (
-    <div>The home page</div>
+    <div>
+      <h1>The Home Page</h1>
+      <form onSubmit={submitFormHandler}>
+        <div>
+          <label htmlFor="email">Your Email Address</label>
+          <input type="email" id="email" ref={emailInputRef} />
+        </div>
+        <div>
+          <label htmlFor="feedback">Your Feedback</label>
+          <textarea id="feedback" rows="5" ref={feedbackInputRef}></textarea>
+        </div>
+        <button>Send Feedback</button>
+      </form>
+    </div>
   )
 }
 
